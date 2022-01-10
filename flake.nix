d9d9d25a46d4fb0f9695b6b8a6dea9139c319053
madisetti <madisetti@jhu.edu>
2022-01-10 13:03:28 -0500

    maybe it just ins't meant to be...

--- flake.nix
+++ flake.nix
@@ -20,13 +20,13 @@
           # what it's convenient and removes a moving piece.
           outputHashMode = "recursive";
           outputHashAlgo = "sha256";
-          outputHash = "sha256-sNa5VLMJ8RDHevMtTmP5ttedcf0NerBJeYlKkCLyI6k=";
+          outputHash = "sha256-f6lORzSxzlQpTS87jx1yjiscTBl/k4WRFklTaHjQl4c=";
           # Use source
           src = self;
           # We need unzip to build this package
           buildInputs = [ pkgs.nodePackages.node2nix ];
           buildPhase = ''
-            node2nix --development -l package-lock.json
+            node2nix --development --strip-optional-dependencies -l package-lock.json
           '';
           # Installing simply means copying all files to the output directory
           installPhase = ''# Build source files and copy them over.
